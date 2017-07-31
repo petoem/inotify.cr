cbe2ed4127593f637c5fe0cf0091174fc2c4692e
Michael Petö <michael@petoe.me>
2017-07-31 17:20:57 +0200

    Add File.watch and Dir.watch methods

--- src/inotify.cr
+++ src/inotify.cr
@@ -1,1 +1,20 @@
 require "./inotify/*"
+
+module Inotify
+  def self.watch(path : String, recursive : Bool = false, &block : Event ->)
+    # TODO: check if platform is linux
+    Watcher.new(path, recursive, &block)
+  end
+end
+
+class File
+  def self.watch(path : String, &block : Inotify::Event ->)
+    Inotify.watch(path, false, &block)
+  end
+end
+
+class Dir
+  def self.watch(path : String, recursive : Bool = false, &block : Inotify::Event ->)
+    Inotify.watch(path, recursive, &block)
+  end
+end
