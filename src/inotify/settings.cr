d40346af6d553e72638783eb8440acc4a1a914cc
Michael Petö <michael@petoe.me>
2017-07-31 17:04:07 +0200

    Move DEFAULT_WATCH_FLAG to settings

--- src/inotify/settings.cr
+++ src/inotify/settings.cr
@@ -1,6 +1,7 @@
 require "logger"
 
 module Inotify
-  LOG = Logger.new(STDOUT)
+  LOG                = Logger.new(STDOUT)
   LOG.level = Logger::Severity.parse(ENV["LOG_LEVEL"]? || "UNKNOWN")
+  DEFAULT_WATCH_FLAG = LibInotify::IN_MOVE | LibInotify::IN_MOVE_SELF | LibInotify::IN_MODIFY | LibInotify::IN_CREATE | LibInotify::IN_DELETE | LibInotify::IN_DELETE_SELF
 end
