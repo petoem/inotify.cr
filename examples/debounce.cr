8c0ff4b06e7d4e6a99e64be04d25183c1bf833b7
Michael Petö <michael@petoe.me>
2017-09-21 20:22:26 +0200

    Add debounce example

---
+++ examples/debounce.cr
@@ -0,0 +1,28 @@
+require "../src/inotify"
+
+class Debounce
+  @waiter : Concurrent::Future(Nil) | Nil
+  def initialize(@path : String, @delay : Time::Span, &block : Deque(Inotify::Event) -> )
+    @block = block
+    @queue = Deque(Inotify::Event).new
+    Inotify.watch @path do |event|
+      @waiter.try &.cancel
+      @queue.push event
+      @waiter = delay @delay.seconds do
+        @block.call @queue.dup
+        @queue.clear
+        nil
+      end
+    end
+  end
+end
+
+Debounce.new "./spec/test", 5.seconds do |events|
+  pp events
+end
+
+# To keep program alive
+loop do
+  sleep 1
+  puts "sleep"
+end
