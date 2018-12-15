44b62f60b58dfb26801aaa54b1a6700174c60af5
Michael Petö <michael@petoe.me>
2018-12-15 13:24:21 +0100

    Various changes - add Dir#watch and File#watch - add Inotify.watcher - disable fallback

--- src/inotify.cr
+++ src/inotify.cr
@@ -7,25 +7,50 @@ require "./inotify/settings"
   require "./inotify/lib_inotify"
   require "./inotify/watcher"
 {% else %}
-  module Inotify
-    alias Watcher = Fallback
-  end
+  # module Inotify
+  #   alias Watcher = Fallback
+  # end
+  
+  # For now only linux
+  # TODO: Rework inotify fallback
+  {{ raise "Inotify is only available on linux platform" }}
 {% end %}
 
 module Inotify
-  def self.watch(path : String, recursive : Bool = false, &block : Event ->)
-    Watcher.new(path, recursive, &block)
+  def self.watcher(recursive : Bool = false) : Inotify::Watcher
+    Watcher.new(recursive)
+  end
+
+  def self.watch(path : String, recursive : Bool = false, &block : Inotify::Event ->) : Inotify::Watcher
+    inotify = Inotify.watcher recursive
+    inotify.on_event &block
+    inotify.watch path
+    inotify
   end
 end
 
 class File
-  def self.watch(path : String, &block : Inotify::Event ->)
-    Inotify.watch(path, false, &block)
+  def self.watch(path : String, &block : Inotify::Event ->) : Inotify::Watcher
+    inotify = Inotify.watch
+    inotify.on_event &block
+    inotify.watch path
+    inotify
+  end
+
+  def watch(&block : Inotify::Event ->) : Inotify::Watcher
+    File.watch @path, &block
   end
 end
 
 class Dir
-  def self.watch(path : String, recursive : Bool = false, &block : Inotify::Event ->)
-    Inotify.watch(path, recursive, &block)
+  def self.watch(path : String, recursive : Bool = false, &block : Inotify::Event ->) : Inotify::Watcher
+    inotify = Inotify.watch recursive
+    inotify.on_event &block
+    inotify.watch path
+    inotify
+  end
+
+  def watch(recursive : Bool = false, &block : Inotify::Event ->) : Inotify::Watcher
+    Dir.watch @path, recursive, &block
   end
 end
